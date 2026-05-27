db97297921fc0a7c842ea01d3d41c525286648d0
JoaoDev-Pro <moraisvictorpro@gmail.com>
2026-05-26 21:50:23 -0300

    Primeiro commit: Backend estruturado

---
+++ server.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.status(200).json(users)
+})
+
+app.post('/usuarios', async (req, res) => {
+
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    console.log(user)
+
+    res.status(201).json(user)
+})
+
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    res.status(200).json({ message: "Usuario atualizado com sucesso!" })
+})
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({ message: "Usuario excluido com sucesso!" })
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
+
